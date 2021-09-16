485c29c976a770ff0e40933d0b3b84c5edb63314
Ismaele Giuseppe Recca <sc19igr@leeds.ac.uk>
2021-09-16 14:14:32 +0200

    Created DinoGame
    Added all files relevant to the Dino Game

--- src/main.c
+++ src/main.c
@@ -1,50 +1,188 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+#define SCREEN_WIDTH (1280)
+#define SCREEN_HEIGHT (720)
 
 // Change this depending on the path of your executable relative to the assets folder
-#define ASSET_PATH "assets/"
+#define ASSET_PATH "../assets/"
+
+typedef enum enemy_flags_enum
+{
+	ENEMY_ACTIVE = 0x01,
+	ENEMY_PASTUS = 0x02,
+	ENEMY_PASTMIDDLE = 0x04
+} EnemyFlags;
+
+typedef struct enemy_state_struct
+{
+	EnemyFlags flags;
+	union {
+		Rectangle collider;
+		struct
+		{
+			float x;
+			float y;
+			float width;
+			float height;
+		};
+	};
+} Enemy;
+#define ENEMY_COUNT (3)
+Enemy enemies[ENEMY_COUNT];
+
+#define GROUND_HEIGHT (30)
+int GetHeighOnGround(int height)
+{
+	return SCREEN_HEIGHT - (GROUND_HEIGHT + height);
+}
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
-
-    Texture2D texture = LoadTexture(ASSET_PATH"test.png");
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
+	
+	const Rectangle standDino = {
+			.x = 30,
+			.width = 50,
+			.height = 80,
+			.y = GetHeighOnGround(80)
+	};
+	const Rectangle crouchDino = {
+			.x = 30,
+			.width = 75,
+			.height = 40,
+			.y = GetHeighOnGround(50)
+	};
+	
+	const Rectangle cactiCollider = {
+			.x = SCREEN_WIDTH,
+			.width = standDino.width - 8,
+			.height = standDino.height - 20,
+			.y = GetHeighOnGround(standDino.height - 20)
+	};
+	const Rectangle pteroCollider = {
+			.x = SCREEN_WIDTH,
+			.width = standDino.width + 70,
+			.height = 40,
+			.y = GetHeighOnGround(40 + 56)
+	};
+	
+	Rectangle dinoRect = standDino;
+	float dinoSpeed = 0.0f;
+	
+	for (int i = 0; i < ENEMY_COUNT; i++)
+	{
+		enemies[i].collider = cactiCollider;
+	}
+	enemies[0].flags |= ENEMY_ACTIVE;
+	
+	int score = 0;
+	bool gameOver = false;
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+    	if (!gameOver)
+		{
+			if (IsKeyPressed(KEY_SPACE) && dinoRect.y == GetHeighOnGround(dinoRect.height))
+			{
+				dinoSpeed = -4.8f;
+			}
+			
+			if (IsKeyPressed(KEY_C))
+			{
+				//dinoRect.y = dinoRect.y + (standDino.y - crouchDino.y);
+				dinoRect.y = dinoRect.y + (crouchDino.y - standDino.y);
+				dinoRect.width = crouchDino.width;
+				dinoRect.height = crouchDino.height;
+			} else if (IsKeyReleased(KEY_C))
+			{
+				//dinoRect.y = dinoRect.y - (standDino.y - crouchDino.y);
+				dinoRect.y = dinoRect.y - (crouchDino.y - standDino.y);
+				dinoRect.width = standDino.width;
+				dinoRect.height = standDino.height;
+			}
+		
+			if (dinoSpeed < 5.0f)
+			{
+				dinoSpeed += 5 * GetFrameTime();
+			} else
+			{
+				dinoSpeed = 5.0f;
+			}
+			dinoRect.y += dinoSpeed;
+		
+			if (dinoRect.y >= GetHeighOnGround(dinoRect.height))
+			{
+				dinoRect.y = GetHeighOnGround(dinoRect.height);
+			}
+			
+			for (int i = 0; i < ENEMY_COUNT; i++)
+			{
+				if (enemies[i].flags & ENEMY_ACTIVE)
+				{
+					if (enemies[i].x + enemies[i].width < dinoRect.x && !(enemies[i].flags & ENEMY_PASTUS))
+					{
+						enemies[i].flags |= ENEMY_PASTUS;
+						score++;
+					}
+					
+					gameOver = gameOver | CheckCollisionRecs(dinoRect, enemies[i].collider);
+					
+					enemies[i].x -= 150 * GetFrameTime();
+					
+					if (enemies[i].x + enemies[i].width < 0)
+						enemies[i].flags &= ~ENEMY_ACTIVE;
+					
+					if (enemies[i].x - enemies[i].width < SCREEN_WIDTH / 3 && !(enemies[i].flags & ENEMY_PASTMIDDLE))
+					{
+						enemies[i].flags |= ENEMY_PASTMIDDLE;
+						for (int j = 0; j < ENEMY_COUNT; j++)
+						{
+							if (j != i && !(enemies[j].flags & ENEMY_ACTIVE))
+							{
+								int pteroORcacti = GetRandomValue(0, 1);
+								
+								enemies[j] = (Enemy){0};
+								enemies[j].flags |= ENEMY_ACTIVE;
+								
+								if (pteroORcacti)
+									enemies[j].collider = cactiCollider;
+								else
+									enemies[j].collider = pteroCollider;
+									
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+    	
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+        
+        DrawRectangle(0, SCREEN_HEIGHT - GROUND_HEIGHT, SCREEN_WIDTH, GROUND_HEIGHT, BROWN);
+        DrawRectangleRec(dinoRect, BLACK);
+        for (int i = 0; i < ENEMY_COUNT; i++)
+		{
+        	if (enemies[i].flags & ENEMY_ACTIVE)
+				DrawRectangleRec(enemies[i].collider, GRAY);
+		}
+        
+        if (gameOver)
+		{
+			DrawText("Game Over!", 30, 30, 60, BLACK);
+		} else
+		{
+			DrawText(FormatText("Score: %i", score), 30, 30, 60, BLACK);
+		}
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();
 
     return 0;
 }
